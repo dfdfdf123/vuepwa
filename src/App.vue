b57daa91cb0788f5887735f30aac206aa0f66128
johnromanoff <johnromanoff@yandex.ru>
2019-03-10 16:11:42 +0300

    create components

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <main></main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Panel from './components/Panel.vue';
+import InputForm from './components/InputForm.vue';
+import Result from './components/Result.vue';
+import { calcTaxes } from './calc.js';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Panel,
+    InputForm,
+    Result
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style lang="scss" src="../assets/styles/InputForm.scss"/>
